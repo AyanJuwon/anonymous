44a0964388ecd9bfde13ff6389f66ea7a67cc184
Ayanniran Oluwajuwon <oiayanniran@student.lautech.edu.ng>
2021-07-26 16:04:20 +0100

    fixed models

--- djangoAnonymous/core/models.py
+++ djangoAnonymous/core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Message(models.Model):
+    
+    # message
+    # autoslug with username nahh
+    # create view that shows dashboard to only logged in users,
+    
+    
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    # message_id = models.IntegerField(auto_created=),
+    message = models.TextField(max_length=256,)
+    time = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return f"{self.message}-{self.user}"
